d93efec6262b671386cb54c0faa4782563c34b57
Work <vladfomin548@gmail.com>
2024-03-06 12:41:44 +0300

    добавление action для тестов

--- tests/test_api_get_fruits_all.py
+++ tests/test_api_get_fruits_all.py
@@ -1,9 +1,11 @@
 from tests.API.api import ApiTest
 from schemas.Get_fruits import valid_schema
+import os
 
 
 class TestGetAllFruts:
 
     def test_get_fruits_all(self):
         response = ApiTest('/api/fruit/all').fruts_get(valid_schema)
+        print(os.name)
         assert response.status == 200
